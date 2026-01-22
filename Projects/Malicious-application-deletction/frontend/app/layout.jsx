2bbdfb852c046e71f69c1ee5697d06192bbd7c04
Shubham Raj <shubmrj@gmail.com>
2026-01-22 18:30:56 +0530

    add frontend work which done by my friend

---
+++ Projects/Malicious-application-deletction/frontend/app/layout.jsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Banking APK Detector - MP Police",
+  description: "AI-powered detection system for fake banking applications",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
